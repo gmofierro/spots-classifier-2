11ccf8d79c65e2f282bd76363bca6a60220d6ff6
José Guillermo Fierro Mendoza <guillermo.fierro@itcelaya.edu.mx>
2024-07-15 09:28:12 -0600

    configuración inicial del proyecto

---
+++ app.py
@@ -0,0 +1,107 @@
+import csv
+#from io import BytesIO
+import streamlit as st
+from streamlit_option_menu import option_menu
+#from dev import *
+from spots_classifier import SpotsClassifier
+import pandas as pd
+
+st.set_page_config(page_title="Page Title", layout="wide")
+
+#st.set_page_config(page_icon="📊", page_title=" -- Spots Classifier -- !", layout="wide")
+st.image(".\images\imagen_portada.png", width=200)
+st.title("Determinación del alcance y tarifas de anuncios de TV")
+
+#df_test3 = pd.DataFrame()
+
+def load_file():        
+    c30, c31 = st.columns([8, 1]) # 3 columnas: 10%, 60%, 10%
+    with c30:
+        # Bloque 1
+        uploaded_file = st.file_uploader(
+            label="Spots"
+            "", type = 'xlsx',
+            key="1",
+        )
+        
+    return uploaded_file
+
+def export_file():    
+    ##df.to_excel('df_test3.xlsx')
+    #file_name = file.to_excel('df_test3.xlsx')
+        #with open('CONCENTRADO TV AZTECA MAYO FINAL.xlsx', "rb") as template_file:
+    
+    c30, c31 = st.columns([8, 1]) # 3 columnas: 10%, 60%, 10%
+    with c30:
+        with open('df_test3.xlsx', "rb") as template_file:
+            template_byte = template_file.read()
+            st.download_button(label="Click to Download Template File", data=template_byte, file_name="template.xlsx", mime='application/octet-stream')  
+        return 0
+
+
+def analice():
+    c30, c31 = st.columns([8, 1]) # 3 columnas: 10%, 60%, 10%
+    with c30:
+        uploaded_file = load_file()
+        if uploaded_file is not None:
+            file_name = uploaded_file.name
+            st.write(f'file: {file_name}')
+
+            # Bloque 2a
+            info_box_wait = st.info('Realizando la clasificación...', icon="ℹ️")
+            
+            file_name = uploaded_file.name
+            
+            #st.write(f'file: {file_name}')
+            
+            clasificador = SpotsClassifier(file_name)
+    
+            clasificador.initial_configuration()
+
+            st.write('Actualizando el alcance ..')
+            if clasificador.actualiza_alcance_eventos() == 0:
+                st.write('Actualización del alcance OK..')
+
+            ## Se tiene en df_test3 los registros con el ALCANCE ya determinado
+            ## Una vez que ya se determinó el ALCANCE se hace la..
+            ## Implementación para cálculo de las tarifas 
+
+            clasificador.configurar_archivos_para_tarifas() ## se lee el archivo de excel con la tarifas por sede y los canales por Sede
+
+            clasificador.actualiza_tarifa()
+            
+            st.write('Se concluyó la actualización de las tarifas')
+            
+            st.write('---')
+
+            clasificador.export_to_excel()
+            #df_xlsx = to_excel(df_test3)
+        else: 
+            st.write('Sube el archivo de excel ..')
+        
+        st.write('---')
+    return 0   
+
+
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Run process', 'Download Result File'], 
+        icons=['house', 'gear', ['play']], menu_icon="cast", default_index=0)
+    selected
+    
+
+if selected == 'Run process':
+        df = analice()    ## incluye cargar el archivo
+        #GFM_1 export_file()
+        # Bloque 2
+
+if selected == 'Download Result File':
+    c30, c31 = st.columns([8, 1]) # 3 columnas: 10%, 60%, 10%
+    with c30:
+        st.write('')
+        st.write('')
+        export_file()
+        st.write('---')
+        # Bloque 2
+
+
+
---
+++ spots_classifier.py
@@ -0,0 +1,440 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+class SpotsClassifier:
+  def __init__(self, cadena):
+    self.lista_nacionales=[]
+    self.lista_asociados=[]
+    self.lista_locales=[]
+
+    self.df_test = pd.DataFrame() 
+    self.df_test3 = pd.DataFrame()
+    self.df_spots_fecha_ordenado = pd.DataFrame()
+    self.df_canales_nacionales = pd.DataFrame()
+    self.df_canales_asociados = pd.DataFrame()
+    self.df_canales_locales = pd.DataFrame()
+
+    #ruta = "/content/drive/MyDrive/proy_anun/"
+    self.ruta = "./data/"  #dev\utils\spots_classifier.py
+
+    ## self.fileName = self.ruta + "CONCENTRADO TV AZTECA MAYO FINAL_GFM_4000.xlsx"
+
+    self.fileName = self.ruta + cadena
+ 
+    ## para obtener las tarifas
+    self.df_tarifas = pd.DataFrame()   # El Dataframe para recuperar las tarifas por plazas
+    self.df_plazas_canales = pd.DataFrame()   # El Dataframe para recuerar las los canales por plazas
+    self.df_tarifas_nacionales = pd.DataFrame()  # El Dataframe para recuperar las tarifas nacionales
+
+    #self.fileName = cadena
+
+  def initial_configuration(self):
+
+    df = pd.read_excel(self.fileName, sheet_name='Hoja1')
+    self.df_test = df
+
+    self.configura_db_test_fechas()
+  
+    self.filtra_df_spots()
+
+    self.ordenar_spots_fecha()
+
+
+    return 0
+
+  def print_initial_condition(self):
+    print(self.fileName)
+    print(self.lista_nacionales)
+    print(self.lista_asociados)
+    print(self.lista_locales)
+    
+    
+    #print(self.df_test3)
+    print (self.df_spots_fecha_ordenado)
+    ## print(self.df_canales_nacionales)
+
+    return 0
+
+  def configura_db_test_fechas(self):
+    self.df_test['FECHA_NEW'] = self.df_test.FECHA.astype(str) + " " + self.df_test.HORA.astype(str)
+    # convert the 'Date' column to datetime format -- usando la funcio datetime de pandas
+
+    self.df_test['FECHA_NEW'] = pd.to_datetime(self.df_test['FECHA_NEW'], format='mixed')
+
+    self.df_test2 = pd.DataFrame(self.df_test.DURACION.str.split(':').tolist(), columns = ['dur_min','dur_seg'])
+    #dfq1['new'] = dfq1['DURACION'].str[3:5]
+
+    self.df_test2['dur_min'] = self.df_test2['dur_min'].astype(int)*1
+    self.df_test2['dur_seg'] = self.df_test2['dur_seg'].astype(int)*1
+
+    self.df_test3 = pd.concat([self.df_test, self.df_test2], axis=1)
+    #print(df_test3)
+
+    print('Configura_fechas_OK')
+
+    return 0 ## self.df_test3
+
+  ## Generar un dataFrame auxiliar para manipular solo el CANAL, VERSION (Spot) y la FECHA en formato datetime
+  ## Integrar en un dataset CANAL, VERSION (El Anuncio) y la FECHA COMPLETA
+  ## df_test_articulos_canales_fechas
+
+  def filtra_df_spots(self):
+    df_test_spots_canales_fechas = self.df_test3[['CANAL', 'VERSION', 'FECHA_NEW']]
+
+    ## df_spots_fecha_ordenado es el dataframe con los tres atributos ORDENADO por VERSION y FECHA_NEW
+
+    #df_spots_fecha_ordenado = df_test_spots_canales_fechas.sort_values(['VERSION','FECHA_NEW'])
+
+    self.df_spots_fecha_ordenado = df_test_spots_canales_fechas.sort_values(['VERSION','FECHA_NEW'])
+    self.df_spots_fecha_ordenado.sort_values(['VERSION','FECHA_NEW'], ascending=True, inplace= True)
+
+
+    print('OK... filtra_df-spots')
+    return 0 ## df_spots_fecha_ordenado
+
+  def ordenar_spots_fecha(self):
+    df_test_spots_canales_fechas = self.df_test3[['CANAL', 'VERSION', 'FECHA_NEW']]
+    #df_test_spots_canales_fechas.sort_values(['VERSION','FECHA_NEW'], ascending=True, inplace= True)
+    df_test_spots_canales_fechas.sort_values(['VERSION','FECHA_NEW'], ascending=True)
+    self.df_spots_fecha_ordenado = df_test_spots_canales_fechas
+
+    return 0
+
+  ## Definir para el calculo de la diferencia de dos fechas en segundos
+  def dif_fechas_segundos_fechas(self, fecha1, fecha2):
+    dif = fecha2 - fecha1
+    result = dif.days * 24 * 3600 + dif.seconds
+    return result
+
+  ## Definir para el calculo de la diferencia de dos fechas en segundos integrando un margen en segundos
+  def dif_fecha_con_margen_segundos_fechas(self, fecha1, fecha2, margen):
+    dif = abs(fecha2 - fecha1)
+    result = dif.days * 24 * 3600 + dif.seconds
+    result =  result    #abs(result - margen)
+    #print(result)
+    return result
+
+  def adicionar_en_lista_nacionales(self, canal, version, fecha):
+    flag = 0
+    reg2 = [canal, version, fecha] in self.lista_asociados
+
+    if reg2:
+      flag = 0  # ya no se hace nada .. existe en asociados..
+    else:
+      reg2 = [canal, version, fecha] in self.lista_nacionales
+      if reg2:
+        flag = 1 # entonces insertar en asociados
+      else:
+        self.lista_nacionales.append([canal, version, fecha])
+        flag = 0      # se insertó en nacionales  NO insertar en asociados
+
+      return flag
+
+  def adicionar_en_lista_asociados(self, canal, version, fecha):
+    out = 0
+    reg2 = [canal, version, fecha] in self.lista_asociados
+    if not reg2:
+      self.lista_asociados.append([canal, version, fecha])
+      out = 1
+    else:
+      out = -1 
+
+    return out
+
+  def adicionar_en_lista_nacionales_v2(self, canal, version, fecha):
+    out = 0
+    reg2 = [canal, version, fecha] in self.lista_asociados
+    if not reg2:
+      reg3 = [canal, version, fecha] in self.lista_nacionales
+      if not reg3:
+        self.lista_nacionales.append([canal, version, fecha])
+        out = 1
+      else:
+        out = -1 
+
+    return out
+
+  ##  --
+  def actualiza_alcance_para_nacionales(self):
+    for i in range(len(self.df_test3)):
+      canal = self.df_test3.loc[i,'CANAL']
+      version = self.df_test3.loc[i,'VERSION']
+      fecha = self.df_test3.loc[i,'FECHA_NEW']
+      if [canal, version, fecha] in self.lista_nacionales:
+        self.df_test3.loc[i,'SELECCIÓN'] = 'NACIONAL'
+    print('OK .. Actualizado nacionales')
+
+  # actualizar el alcance para Asociados
+  def actualiza_alcance_para_asociados(self):
+    for i in range(len(self.df_test3)):
+      canal = self.df_test3.loc[i,'CANAL']
+      version = self.df_test3.loc[i,'VERSION']
+      fecha = self.df_test3.loc[i,'FECHA_NEW']
+      if [canal, version, fecha] in self.lista_asociados:
+        self.df_test3.loc[i,'SELECCIÓN'] = 'N'
+
+    print ('OK .. Actualizado Asociados')   
+    return 0
+  # ---------------
+  #Actualiza el alcance para los registros locales
+  def actualiza_alcance_para_locales(self):
+    for i in range(len(self.df_test3)):
+      canal = self.df_test3.loc[i,'CANAL']
+      version = self.df_test3.loc[i,'VERSION']
+      fecha = self.df_test3.loc[i,'FECHA_NEW']
+      alcance = self.df_test3.loc[i,'SELECCIÓN']
+      if alcance == '':
+        #print ([canal, version, fecha])
+        self.df_test3.loc[i,'SELECCIÓN'] = 'LOCAL'
+      ## else:
+        ## print(f'elemento: {[canal, version, fecha]} NO es LOCAL..')
+    print('OK..LOCALES')
+
+    return 0
+
+  ## ------------- determina el alcance ------------
+  def determina_nivel_alcance(self):
+    self.lista_nacionales.clear()
+    self.lista_asociados.clear()
+
+    df_global = self.df_spots_fecha_ordenado[['CANAL', 'VERSION', 'FECHA_NEW']]
+    df_global = df_global.sort_values(['VERSION', 'FECHA_NEW'], ascending = True)
+
+    nDatos = len(df_global)
+    #nDatos = 200
+
+    bande = False
+
+    ## f = open('resultados4.txt','a') para cuando se necesite depurar
+    #for i in range(0, nDatos):
+    i = 0
+    while i in range(0, nDatos):
+      #print(f'i= {i}')
+      loc_n = df_global.index[i]
+      canal1 = df_global.loc[loc_n,'CANAL']
+      version1 = df_global.loc[loc_n, 'VERSION']
+      fecha1 = df_global.loc[loc_n,'FECHA_NEW']
+
+      df_w1 = self.df_spots_fecha_ordenado.VERSION == version1
+      df_w1 = self.df_spots_fecha_ordenado[df_w1]
+      ## revisando
+      df_w1 = df_w1.sort_values(['FECHA_NEW'], ascending=True)
+
+      n_df = len(df_w1)
+      k = 0
+      
+      # for j in range(i+1, num_df_filtrado):
+
+      while k < n_df:
+        loc_n = df_w1.index[k]
+        canal1 = df_w1.loc[loc_n,'CANAL']
+        version1 = df_w1.loc[loc_n, 'VERSION']
+        fecha1 = df_w1.loc[loc_n,'FECHA_NEW']
+        j = k + 1 #  = k + 1
+        bande = False
+        bande_asociados = False
+        while (j < n_df):
+          loc = df_w1.index[j]
+          canal2 = df_w1.loc[loc,'CANAL']
+          version2 = df_w1.loc[loc,'VERSION']
+          fecha2  = df_w1.loc[loc, 'FECHA_NEW']
+
+          dif_fecha = self.dif_fecha_con_margen_segundos_fechas(fecha1, fecha2, 120)
+          flag_fecha = dif_fecha <= 120
+
+          flag_canal = (canal1 == canal2)
+          flag_version = version1 == version2
+
+          es_nacional = version1 == version2 and flag_fecha
+
+          # inicio del algoritmo
+
+          if es_nacional:
+            flag_n = self.adicionar_en_lista_nacionales_v2(canal1, version1, fecha1)
+            if flag_n == 1 or flag_n == -1 :  ## si se pudo adicionar a 
+              self.adicionar_en_lista_asociados(canal2, version2, fecha2)
+            
+          # fin de lógica de detección de nacionales y asociados
+          j = j + 1
+        k = k + 1
+      
+      if i % 150 == 0:
+        print(f'.{i}.')
+      else:
+        print (f'.{i}.', end="")  
+      i = i + n_df
+
+    #imprime la lista
+    print('-------------- lista de asociados -------------')
+    #print(lista_asociados)
+        #imprime la lista
+    print('-------------- lista de Nacionales -------------')
+    #print(lista_nacionales)
+
+    return 0
+
+    #### fin determina nivdel de alcance
+
+  ## método que exporta los resultados a archivo de excel
+  def export_to_excel(self):
+    ## export el archivo df_test3 a excel
+    self.df_test3.to_excel('df_test3.xlsx')
+    print('Se exportó el archivo de resultados: .. df_test3.xlsx')
+    return self.df_test3
+
+  def actualiza_alcance_eventos(self):
+    ## GFM_1 self.df_test3.loc[:, 'SELECCIÓN'] = ""
+    self.df_test3['SELECCIÓN'] = None
+    #self.df_test3=self.df_test3['SELECCIÓN'].astype(str)
+    self.df_test3.loc[:, 'SELECCIÓN'] = ""
+    self.determina_nivel_alcance()
+    self.actualiza_alcance_para_nacionales()
+    self.actualiza_alcance_para_asociados()
+    self.actualiza_alcance_para_locales()
+
+
+#### Fin de métodos para implementar el Algoritmo para determinar el ALCANCE
+
+  def configurar_archivos_para_tarifas(self):
+    filename_plazas_tarifas = self.ruta + "tarifas_GFM.xlsx"
+    self.df_tarifas = pd.read_excel(filename_plazas_tarifas, sheet_name='PLAZAS_TARIFAS')
+
+    #filename_plazas_canales = "/content/drive/MyDrive/proy_anun/tarifas_GFM.xlsx"
+    self.df_plazas_canales = pd.read_excel(filename_plazas_tarifas, sheet_name='PLAZAS_CANALES')
+
+    self.df_tarifas_nacionales = pd.read_excel(filename_plazas_tarifas, sheet_name='TARIFAS_NACIONALES')
+
+
+
+    return 0
+## fin del métod para configurar archivos para Tarifas
+
+## método para ubicar la tarifa de acuerdo a la PLAZA
+  def busca_tarifa_por_sede_hora(self, plaza, hora):
+    cond1 = plaza == self.df_tarifas.PLAZA           # ['PLAZA']
+    cond2 = hora >= self.df_tarifas.HOR_INI          # ['HOR_INI']
+    cond3 = hora < self.df_tarifas.HOR_FIN                 #['HOR_FIN']
+
+    df_filtrado_tarifa = self.df_tarifas[['PLAZA', 'TARIFA_SPOT']].query('@cond1 & @cond2 & @cond3')
+    result = df_filtrado_tarifa.TARIFA_SPOT.values[0]
+    #print(result)
+
+    return result
+## fin del método
+
+## método para buscar la plaza de acuerdo al CANAL que aplique
+  def busca_plaza_por_canal(self, canal):
+    cond1 = canal == self.df_plazas_canales.CANAL
+    df_filtrado_plaza = self.df_plazas_canales[cond1]
+    #print(df_filtrado_plaza['PLAZA'])
+    plaza =  df_filtrado_plaza.PLAZA.values[0]    ###['PLAZA']                           #where[cond1 and cond2 and cond3]
+    #print(f'Plaza : {plaza}')
+    return plaza
+## fin del método
+
+## método para determinar la tarifa para un evento NACIONAL de acuerdo a la HORA
+
+  def determina_tarifa_en_nacional_hora(self, hora):
+    plaza = 'NACIONAL'
+    cond1 = plaza == self.df_tarifas_nacionales.PLAZA           # ['PLAZA']
+    cond2 = hora >= self.df_tarifas_nacionales.HOR_INI          # ['HOR_INI']
+    cond3 = hora < self.df_tarifas_nacionales.HOR_FIN                 #['HOR_FIN']
+
+    df_filtrado_tarifa_nal = self.df_tarifas_nacionales[['TARIFA_SPOT']].query('@cond1 & @cond2 & @cond3')
+    result = df_filtrado_tarifa_nal.TARIFA_SPOT.values[0]
+    #print(result)
+
+    return result
+## fin del método
+
+# Método para recuperar la Tarifa usando el CANAL como argumento
+  def determina_tarifa_local(self, canal, hora):
+    plaza = self.busca_plaza_por_canal(canal)
+    tarifa_x = self.busca_tarifa_por_sede_hora(plaza, hora)
+    return tarifa_x
+## fin del método
+
+# Función para recuperar la Tarifa usando el CANAL como argumento
+  def nacional(self, hora):
+    tarifa_x = self.busca_tarifa_en_nacional_hora(hora)
+    ##print(f'Tarifa a aplicar: {tarifa_x}')
+    return tarifa_x
+## fin del método
+
+## método para obtener el factor a aplicar dependiendo del rango de la duración
+  def determina_factor_aplicar_tarifa(self, duracion):
+    if duracion <= 10:
+      factor = 0.5
+    elif duracion > 10 and duracion <= 20:
+      factor = 1.0
+    elif duracion > 20 and duracion <= 30:
+      factor = 1.5
+    elif duracion > 30 and duracion <= 40:
+      factor = 2
+    elif duracion > 40:
+      factor = 2.5
+    return factor
+## fin del método
+
+## método integrador para actualiza la tarifa de cada uno de los registros
+  def actualiza_tarifa(self):
+    for i in range(len(self.df_test3)):
+      canal = self.df_test3.loc[i,'CANAL']
+      version = self.df_test3.loc[i,'VERSION']
+      fecha = self.df_test3.loc[i,'FECHA_NEW']
+      alcance = self.df_test3.loc[i,'SELECCIÓN']
+      hora = fecha.hour
+
+      #print(f'CANAL: {canal} -- VERSION: {version} hora: {hora}')
+
+      duracion = self.df_test3.loc[i,'dur_min']*60 + self.df_test3.loc[i,'dur_seg']
+      factor = self.determina_factor_aplicar_tarifa(duracion)
+
+      if alcance == 'NACIONAL':
+          #print (f'Alcance:: {alcance} -- hora: {hora}')
+        if  i% 150 == 0:
+          print('*')
+        else:
+          print('*', end="")
+
+        self.df_test3.loc[i,'TARIFA'] = self.determina_tarifa_en_nacional_hora(hora) * factor
+
+      elif alcance == 'LOCAL':
+
+        if  i% 150 == 0:
+          print('.')
+        else:
+          print('.' , end="")
+
+        self.df_test3.loc[i,'TARIFA'] = self.determina_tarifa_local(canal, hora) * factor
+
+    print('OK..tarifas actualizadas')
+    return self.df_test3
+## fin del método
+
+### ============ FIN DE LA CLASE =================
+ 
+def main():
+  str_file_name = input("ingrese el nombre del archivo: ")
+
+  clasificador = SpotsClassifier(str_file_name)
+ 
+  clasificador.initial_configuration()
+
+  clasificador.actualiza_alcance_eventos()
+
+  ## Se tiene en df_test3 los registros con el ALCANCE ya determinado
+  ## Una vez que ya se determinó el ALCANCE se hace la..
+  ## Implementación para cálculo de las tarifas 
+
+  clasificador.configurar_archivos_para_tarifas() ## se lee el archivo de excel con la tarifas por sede y los canales por Sede
+
+  clasificador.actualiza_tarifa()
+
+  ## clasificador.export_to_excel()
+
+
+
+if __name__ == "__main__":
+  main()
